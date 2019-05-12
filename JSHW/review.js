315aee8ff95feb3be5c69f5eae38e647589236b3
Kenhayes7 <50380106+Kenhayes7@users.noreply.github.com>
2019-05-12 14:10:07 -0500

    Add files via upload

---
+++ JSHW/review.js
@@ -0,0 +1,41 @@
+var me = 'Kendra';
+var age = 25;
+var question = true;
+console.log(me);
+console.log(age);
+console.log(question);
+
+var equation1 = 2 * 2;
+var equation2 = 2 * 4;
+var equation3 = equation1 + equation2;
+console.log(equation1);
+console.log(equation2);
+console.log(equation3);
+
+var math = [equation1, equation2, equation3];
+console.log(math);
+
+math.push(16);
+math.push(20);
+console.log(math);
+
+var pronouns = ['he','she','me', 'you','they', 'them'];
+console.log(pronouns);
+
+var pronouns2 = console.log(pronouns.splice(3));
+console.log(pronouns2);
+console.log(pronouns);
+
+var list = [5,10,15,20,25];
+console.log(list);
+list.reverse();
+console.log(list);
+
+var lbs = 5;
+var sentence = `this bookbag weighs ${lbs} pounds.`;
+console.log(sentence); 
+
+console.log(typeof me);
+console.log(typeof age);
+console.log(typeof question);
+
